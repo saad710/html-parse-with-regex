e7243f31c58756e3f76f21598ab1e57b30618050
saad710 <ifrathasnat2@gmail.com>
2021-11-28 17:02:34 +0600

    parse-html-with-regex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,193 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { Moduledata } from './data'
+import parse from 'html-react-parser';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const htmlContent = `<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!--Author      : @arboshiki-->
+<div id="invoice">
+
+    <div class="toolbar hidden-print">
+        <div class="text-right">
+            <button id="printInvoice" class="btn btn-info"><i class="fa fa-print"></i> Print</button>
+            <button class="btn btn-info"><i class="fa fa-file-pdf-o"></i> Export as PDF</button>
+        </div>
+        <hr>
     </div>
-  );
+    <div class="invoice overflow-auto">
+        <div style="min-width: 600px">
+            <header>
+                <div class="row">
+                    <div class="col">
+                        <a target="_blank" href="https://lobianijs.com">
+                            <img src="http://lobianijs.com/lobiadmin/version/1.0/ajax/img/logo/lobiadmin-logo-text-64.png" data-holder-rendered="true" />
+                            </a>
+                    </div>
+                    <div class="col company-details">
+                        <h2 class="name">
+                            <a target="_blank" href="https://lobianijs.com">
+                            Arboshiki
+                            </a>
+                        </h2>
+                        <div>455 Foggy Heights, AZ 85004, US</div>
+                        <div>(123) 456-789</div>
+                        <div>company@example.com</div>
+                    </div>
+                </div>
+            </header>
+            <main>
+                <div class="row contacts">
+                    <div class="col invoice-to">
+                        <div class="text-gray-light">INVOICE TO:</div>
+                        <h2 class="to">John Doe</h2>
+                        <div class="address">796 Silver Harbour, TX 79273, US</div>
+                        <div class="email"><a href="mailto:john@example.com">john@example.com</a></div>
+                    </div>
+                    <div class="col invoice-details">
+                        <h1 class="invoice-id">INVOICE 3-2-1</h1>
+                        <div class="date">Date of Invoice: 01/10/2018</div>
+                        <div class="date">Due Date: 30/10/2018</div>
+                    </div>
+                </div>
+                <table border="0" cellspacing="0" cellpadding="0">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th class="text-left">DESCRIPTION</th>
+                            <th class="text-right">HOUR PRICE</th>
+                            <th class="text-right">HOURS</th>
+                            <th class="text-right">TOTAL</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="no">04</td>
+                            <td class="text-left"><h3>
+                                <a target="_blank" href="https://www.youtube.com/channel/UC_UMEcP_kF0z4E6KbxCpV1w">
+                                Youtube channel
+                                </a>
+                                </h3>
+                               <a target="_blank" href="https://www.youtube.com/channel/UC_UMEcP_kF0z4E6KbxCpV1w">
+                                   Useful videos
+                               </a> 
+                               to improve your Javascript skills. Subscribe and stay tuned :)
+                            </td>
+                            <td class="unit">$0.00</td>
+                            <td class="qty">100</td>
+                            <td class="total">$0.00</td>
+                        </tr>
+                  //StartTable Notes//       <tr>
+                            <td class="no">01</td>
+                            <td class="text-left"><h3>Website Design</h3>Creating a recognizable design solution based on the company's existing visual identity</td>
+                            <td class="unit">$/Notes.Note_Title/</td>
+                            <td class="qty">30</td>
+                            <td class="total">$1,200.00</td>
+                        </tr> //EndTable Notes//
+                        <tr>
+                            <td class="no">02</td>
+                            <td class="text-left"><h3>Website Development</h3>Developing a Content Management System-based Website</td>
+                            <td class="unit">$40.00</td>
+                            <td class="qty">80</td>
+                            <td class="total">$3,200.00</td>
+                        </tr>
+                        <tr>
+                            <td class="no">03</td>
+                            <td class="text-left"><h3>Search Engines Optimization</h3>Optimize the site for search engines (SEO)</td>
+                            <td class="unit">$40.00</td>
+                            <td class="qty">20</td>
+                            <td class="total">$800.00</td>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colspan="2"></td>
+                            <td colspan="2">SUBTOTAL</td>
+                            <td>$5,200.00</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2"></td>
+                            <td colspan="2">TAX 25%</td>
+                            <td>$1,300.00</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2"></td>
+                            <td colspan="2">GRAND TOTAL</td>
+                            <td>$6,500.00</td>
+                        </tr>
+                    </tfoot>
+                </table>
+                <div class="thanks">Thank you!</div>
+                <div class="notices">
+                    <div>NOTICE:</div>
+                    <div class="notice">A finance charge of 1.5% will be made on unpaid balances after 30 days.</div>
+                </div>
+            </main>
+            <footer>
+                Invoice was created on a computer and is valid without the signature and seal.
+            </footer>
+        </div>
+        <!--DO NOT DELETE THIS div. IT is responsible for showing footer always at the bottom-->
+        <div></div>
+    </div>
+</div>`
+
+  //read_data_with_regex_global_and_case_insensitive_flags
+  const regString = /\/\/StartTable notes\/(\n?.)+\/EndTable notes\/\//gi
+  console.log(regString)
+  const requirePart = regexData(regString)
+  console.log(requirePart)
+
+  //notes_data
+  const findData = Moduledata?.relatedListsData.Notes?.map(
+    (note) => note.Note_Content,
+  )
+  console.log(findData)
+
+  //require-data-which-will-be-changed
+  function regexData(regexStr) {
+    const mainData = regexStr.exec(htmlContent)[0]
+    return mainData
+  }
+
+  //final_output
+  const afterRemoveData = removeExtra(requirePart)
+  console.log(afterRemoveData)
+
+  //remove-unnecessary-things-and-replace-with-main-data
+  function removeExtra(extra) {
+    // const replaceData = extra.replace(/\/\/StartTable Notes\/\/|\/\/EndTable Notes\/\//gi,'')
+    // console.log(replaceData)
+    let titleReplace = []
+    findData.forEach((find) => {
+      titleReplace.push(extra.replace(/\$\/Notes.Note_Title\//gi, find))
+    })
+    console.log(titleReplace)
+    const joinStr = titleReplace.join('')
+    console.log(joinStr)
+    const removeComment = joinStr.replace(
+      /\/\/EndTable Notes\/\/\/\/StartTable Notes\/\//gi,
+      '',
+    )
+    console.log(removeComment)
+    const removeSpace = removeComment.trim()
+    console.log(removeSpace)
+    const replaceFromMainData = htmlContent.replace(requirePart, removeSpace)
+    console.log(replaceFromMainData)
+    return replaceFromMainData;
+  }
+
+  return (
+    <div>
+      {parse(afterRemoveData)}
+    </div>
+  )
 }
 
 export default App;
+
